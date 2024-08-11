10299034b2c063c5c4b7e8d351a022e14fdc96e0
Wyatt James <wyattsamueljames@gmail.com>
2024-08-11 15:52:11 -0600

    chore: update formatting

--- watchdog/item/helpers.py
+++ watchdog/item/helpers.py
@@ -1,6 +1,7 @@
 import requests
 from requests import Response
 
+
 def get_item_page_html(url: str) -> str | None:
     response: Response = requests.get(url)
     if response.status_code == requests.codes.ok:
