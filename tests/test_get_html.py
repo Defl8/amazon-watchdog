ce119f5c79bf2f73ae9bc0510128b9bcd9fae9d1
Wyatt James <wyattsamueljames@gmail.com>
2024-08-11 15:44:52 -0600

    feat: add error test

--- tests/test_get_html.py
+++ tests/test_get_html.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch, Mock
+from requests.models import HTTPError
 
 from watchdog.item.helpers import get_item_page_html
 
@@ -16,6 +17,20 @@ class TestRequestHelpers(unittest.TestCase):
         func_result: str | None = get_item_page_html("https://www.test.com")
 
         self.assertEqual(func_result, "<html>Test Response</html>")
+
+
+    @patch("requests.get")
+    def test_get_item_page_html_error(self, mock_get: Mock):
+        mock_response: Mock = Mock()
+        mock_response.status_code = 404
+        mock_response.raise_for_status.side_effect: HTTPError = HTTPError("HTTP Error")
+
+        mock_get.return_value = mock_response
+
+
+        with self.assertRaises(HTTPError):
+            _: str | None = get_item_page_html("https://www.test.com")
+
 
 
 if __name__ == "__main__":
