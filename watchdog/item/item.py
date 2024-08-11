922751c0fd78328ccc2c6dfd51dad64b9f418684
Wyatt James <wyattsamueljames@gmail.com>
2024-08-11 14:24:08 -0600

    feat: add item class

--- watchdog/item/item.py
+++ watchdog/item/item.py
@@ -1,3 +1,12 @@
 class Item:
-    def __init__():
-        raise NotImplemented
+    def __init__(self, url: str):
+        self.__url = url
+
+    @property
+    def url(self):
+        return self.__url
+
+
+    @url.setter
+    def url(self, url: str):
+        self.__url = url
