6d9aa970e60548dee6ede6535b743a1c9f969c41
Wyatt James <wyattsamueljames@gmail.com>
2024-08-11 15:31:07 -0600

    chore: update type annotations

--- watchdog/item/helpers.py
+++ watchdog/item/helpers.py
@@ -1,9 +1,9 @@
 import requests
 from requests import Response
 
-def get_item_page_html(url: str) -> Response | None:
+def get_item_page_html(url: str) -> str | None:
     response: Response = requests.get(url)
     if response.status_code == requests.codes.ok:
-        return response
+        return response.text
     else:
         response.raise_for_status()
