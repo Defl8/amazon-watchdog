56d66514b7582e9d995f2ae049642691d603eb73
Wyatt James <wyattsamueljames@gmail.com>
2024-08-11 18:01:48 -0600

    feat: add get_item_price

--- watchdog/item/helpers.py
+++ watchdog/item/helpers.py
@@ -1,5 +1,7 @@
+from typing import Any
 import requests
 from requests import Response
+from bs4 import BeautifulSoup, ResultSet
 
 
 def get_item_page_html(url: str) -> str | None:
@@ -8,3 +10,15 @@ def get_item_page_html(url: str) -> str | None:
         return response.text
     else:
         response.raise_for_status()
+
+
+def get_item_price(soup_html: BeautifulSoup) -> str: 
+    PRICE_TAG_TYPE: str = "span"
+    price_classes: list[str] = ["a-price-symbol", "a-price-whole", "a-price-fraction"]
+    price: str = ""
+
+    for index, class_name in enumerate(price_classes):
+        tag: ResultSet[Any] = soup_html.find_all(PRICE_TAG_TYPE, class_=class_name)[0]
+        price += tag.text
+
+    return price
