4fa6d1ab6b553fe96eb124dc994755beed4ed2e4
Wyatt James <wyattsamueljames@gmail.com>
2024-08-13 10:04:48 -0600

    fix: add connection close to functions, add flattening to check if exists table

--- watchdog/db/db.py
+++ watchdog/db/db.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import sqlite3
 from sqlite3 import Connection, Cursor
 
@@ -6,16 +8,19 @@ def connect_to_db(db_name: str) -> Connection:
     return sqlite3.connect(db_name)
 
 
-def make_table(table_name: str, db_name: str, *args: str):
+def make_table(table_name: str, db_name: str, *args: str) -> None:
     column_names: tuple[str, ...] = args
     connection: Connection = connect_to_db(db_name)
     cursor: Cursor = connection.cursor()
     column_string: str = ", ".join(column_names)
     _ = cursor.execute(f"CREATE TABLE {table_name}({column_string})")
+    connection.close()
 
 
 def check_if_exists_table(table_name: str, db_name: str) -> bool:
     connection: Connection = connect_to_db(db_name) 
     cursor: Cursor = connection.cursor()
-    table_list: Cursor = cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    return table_name in table_list.fetchall()
+    table_list: Cursor = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables: list[Any] = list(map(lambda table: table[0], table_list.fetchall()))
+    connection.close()
+    return table_name in tables
